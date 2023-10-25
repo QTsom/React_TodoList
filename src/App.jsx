ceb8f34aa4c78d69f7878228cc6faa8d73657de4
QTsom <somin5998@gmail.com>
2023-10-25 16:59:54 +0900

    feat: add css file, connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Layout from "components/Layout";
 import React, {Component} from "react";
 import HomePage from "containers/Home";
+import resetStyle from "assets/css/reset.css"
+import style from "assets/css/style.css"
 
 class App extends Component {
     render() {
