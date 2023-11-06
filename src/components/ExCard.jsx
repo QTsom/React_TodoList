bb4a4dfaee62de572ed66ed0bc65f59de9e841c9
QTsom <somin5998@gmail.com>
2023-11-06 16:54:23 +0900

    feat: add exCard components

--- src/components/ExCard.jsx
+++ src/components/ExCard.jsx
@@ -1,7 +1,26 @@
+import styled from "styled-components";
+
 const ExCard = () => {
     return (
-        <></>
+        <ExCardContainer>
+            <div className="car-wrap">
+                <div>
+                    <img src="" alt="" />
+                </div>
+
+                <div>
+                    <h2 className="car-wrap__title">title</h2>
+                    <p className="car-wrap__description">description</p>
+                </div>
+
+            </div>
+
+        </ExCardContainer>
     )
 }
 
 export default ExCard;
+
+const ExCardContainer = styled.div`
+    
+`
