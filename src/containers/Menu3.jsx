b6c3920f26974b02c8881a465bd623db88e7fe45
QTsom <somin5998@gmail.com>
2023-11-14 16:43:46 +0900

    feat: add react slick

--- src/containers/Menu3.jsx
+++ src/containers/Menu3.jsx
@@ -1,9 +1,24 @@
+import Slider from "react-slick";
 import Layout from "components/common/Layout";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const MenuPage3 = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+
     return (
         <Layout>
-            
+            <Slider {...settings}>
+                <div>h1</div>
+                <div>bye</div>
+                <div>hello</div>
+            </Slider>
         </Layout>
     );   
 }
