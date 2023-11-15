bd454eb9b70dfb37f86e1ca6c21034b8fc67e08b
QTsom <somin5998@gmail.com>
2023-11-15 13:36:19 +0900

    style: slick navigation, pagination edit style

--- src/containers/Menu3.jsx
+++ src/containers/Menu3.jsx
@@ -20,19 +20,19 @@ const MenuPage3 = () => {
         {
             id: 1,
             title: 'first slick',
-            description: 'first slick description',
+            description: 'cat very cute.',
             thumbnail: slickImg,
         },
         {
             id: 2,
             title: 'second slick',
-            description: 'second slick description',
+            description: 'dogs and cats, two dog, two cat.',
             thumbnail: slickImg2,
         },
         {
             id: 3,
             title: 'third slick',
-            description: 'third slick description',
+            description: 'This parrot has red and green feathers.',
             thumbnail: slickImg3,
         }
     ]
@@ -40,27 +40,76 @@ const MenuPage3 = () => {
 
     return (
         <Layout>
-            <Slider {...settings}>
-                {cardList.map((card, cardItem) => (
-                    <ExCard key={cardItem}>
-                        <div className="car-wrap">
-                            <div>
-                                <img src={card.thumbnail} alt="썸네일" />
+            <SliderWrap>
+                <Slider {...settings}>
+                    {cardList.map((card, cardItem) => (
+                        <ExCard key={cardItem}>
+                            <div className="card-wrap">
+                                <div className="card-wrap__img">
+                                    <img src={card.thumbnail} alt="썸네일" />
+                                </div>
+                                <div className="card-wrap__contents">
+                                    <h2 className="card-wrap__title">{card.title}</h2>
+                                    <p className="card-wrap__description">{card.description}</p>
+                                </div>
                             </div>
-                            <div>
-                                <h2 className="car-wrap__title">{card.title}</h2>
-                                <p className="car-wrap__description">{card.description}</p>
-                            </div>
-                        </div>
-                    </ExCard>
-                ))}
-            </Slider>
+                        </ExCard>
+                    ))}
+                </Slider>
+            </SliderWrap>
         </Layout>
     );   
 }
 
 export default MenuPage3;
 
-const ExCard = styled.div`
+const SliderWrap = styled.div`
+    max-width: 800px;
+    width: 100%;
+    margin: 70px auto;
     
+    .slick-prev {
+        left: -50px;
+    }
+    .slick-next {
+        right: -50px;
+    }
+    .slick-prev,
+    .slick-next {
+        width: 50px;
+        height: 50px;
+    }
+    .slick-prev::before,
+    .slick-next::before {
+        font-size: 35px;
+        color: #fb607a;
+    }
+    .slick-dots li.slick-active button:before {
+        color: #fb607a;
+    }
+    .slick-dots li button:before {
+        color: #fff1f4;
+        opacity: 1;
+    }
+`
+const ExCard = styled.div`
+    width: 100%;
+
+    .card-wrap {
+        border-radius: 20px;
+        overflow: hidden;
+        
+        &__contents {
+            padding: 20px;
+            background-color: #fff1f4;
+        }
+
+        &__title {
+            font-size: 30px;
+            font-weight: 700;
+        }
+        &__description {
+            font-size: 20px;
+        }
+    }
 `
